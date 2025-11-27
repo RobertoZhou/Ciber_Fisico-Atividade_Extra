30a125323e0a4856114f18dd45ed5b5fbf5e72de
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-11-26 22:15:06 -0300

    Iniciando a criação do Buffy

---
+++ Projeto/src/BuffyPrograma.java
@@ -0,0 +1,13 @@
+public class BuffyPrograma {
+
+    // Classe simples representando um programa do arquivo
+    public static class BlocoPrograma {
+        public String nome;
+        public long tamanhoKB;
+
+        public BlocoPrograma(String nome, long tamanhoKB) {
+            this.nome = nome;
+            this.tamanhoKB = tamanhoKB;
+        }
+    }
+}
