5b44a601c04e81119b555b0f0b2eb282a8c641aa
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-11-26 22:16:31 -0300

    Criando Metodos no Buffy

--- Projeto/src/BuffyPrograma.java
+++ Projeto/src/BuffyPrograma.java
@@ -9,5 +9,31 @@ public class BuffyPrograma {
             this.nome = nome;
             this.tamanhoKB = tamanhoKB;
         }
+
+        public static int contarProgramas(BlocoPrograma[] lista) {
+            if (lista == null) {
+                return 0;
+            }
+
+            int qtd = 0;
+            for (int i = 0; i < lista.length; i++) {
+                qtd++;
+            }
+            return qtd;
+        }
+
+        // Lê arquivo TXT e devolve vetor com programas encontrados
+        public static BlocoPrograma[] carregarDoArquivo(String caminho) {
+
+            BlocoPrograma[] vetorTemp = new BlocoPrograma[100]; // limite simples
+            int indice = 0;
+
+            BufferedReader leitor = null;
+
+            try {
+                leitor = new BufferedReader(new FileReader(caminho));
+                String linha;
+            }
+        }
     }
 }
