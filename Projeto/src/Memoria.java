832f241ced0e4016d1864448d6ca10dac1aec2db
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-11-26 21:51:43 -0300

    Iniciando a Classe Memoria

--- Projeto/src/Memoria.java
+++ Projeto/src/Memoria.java
@@ -1,2 +1,9 @@
 public class Memoria {
+    private long tamnhoMemoria;
+    private String disponibilidade;
+    private boolean situacao;
+    private long posicaoComeco;
+    private long comprimento;
+
+
 }
