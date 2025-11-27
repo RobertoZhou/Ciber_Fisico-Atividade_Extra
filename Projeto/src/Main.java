b43478a09f8c0e2a33bc69beea4ee7dbaa185ebe
MatheusDBS <matheusdebortolisilva@gmail.com>
2025-11-27 18:00:40 -0300

    Update Main.java

--- Projeto/src/Main.java
+++ Projeto/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     
     public static void main(String[] args) {
-        String nomeArquivo = "programas.txt";
+        String nomeArquivo = "testes.txt";
         if (args.length > 0) {
             nomeArquivo = args[0];
         }
